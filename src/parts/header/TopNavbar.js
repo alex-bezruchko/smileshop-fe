767e0a4349c35dc98cc7497ab39c9a7a4e45c6bd
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-10-02 11:29:48 -0400

    add toggle navbar menu, add fonts and colors, add brand logo img

--- src/parts/header/TopNavbar.js
+++ src/parts/header/TopNavbar.js
@@ -1,12 +1,50 @@
 import React from "react";
+import houseLogo from "./../../../src/img/herb-house.png";
 import $ from "jquery";
 class TopNavbar extends React.Component {
 
     componentDidMount() {
-
+        var open = false;
         $('#top-navbar-toggle').click(function () {
             $('#top-navbar').slideToggle("fast")
-            $("#top-navbar-toggle").toggleClass('toggle-rotate')
+        })
+        $('.top-navbar-menu-toggle').click(function () {
+            if (open === false) {
+                document.getElementsByClassName('toggle-span')[1].style.display = 'none';
+                document.getElementsByClassName('toggle-span')[0].style.transform = "rotate(-133.33deg)"
+                document.getElementsByClassName('toggle-span')[0].style.width = "35px"
+                document.getElementsByClassName('toggle-span')[0].style.marginLeft = "3px"
+                document.getElementsByClassName('toggle-span')[0].style.marginTop = "0px"
+
+                document.getElementsByClassName('toggle-span')[2].style.transform = "rotate(133.33deg)"
+                document.getElementsByClassName('toggle-span')[2].style.width = "35px"
+                document.getElementsByClassName('toggle-span')[2].style.marginLeft = "3px"
+                document.getElementsByClassName('toggle-span')[2].style.marginTop = "-20px"
+                document.getElementsByClassName('top-navbar-menu')[0].style.display = "flex";
+                document.getElementsByClassName('top-navbar-menu-toggle')[0].style.paddingLeft = "0px"
+                document.getElementsByClassName('top-navbar-menu-toggle')[0].style.paddingRight = "0px"
+                open = true
+            } else {
+                document.getElementsByClassName('toggle-span')[1].style.display = 'flex';
+
+                document.getElementsByClassName('toggle-span')[0].style.transform = "none"
+                document.getElementsByClassName('toggle-span')[0].style.width = "40px"
+                document.getElementsByClassName('toggle-span')[0].style.marginLeft = "0px"
+                document.getElementsByClassName('toggle-span')[0].style.marginTop = "0px"
+
+
+                document.getElementsByClassName('toggle-span')[2].style.transform = "none"
+                document.getElementsByClassName('toggle-span')[2].style.width = "40px"
+                document.getElementsByClassName('toggle-span')[2].style.marginLeft = "0px"
+                document.getElementsByClassName('toggle-span')[2].style.marginTop = "0px"
+
+                document.getElementsByClassName('top-navbar-menu')[0].style.display = "none";
+
+                document.getElementsByClassName('top-navbar-menu-toggle')[0].style.paddingLeft = "0px"
+                document.getElementsByClassName('top-navbar-menu-toggle')[0].style.paddingRight = "0px"
+                open = false
+
+            }
 
         })
 
@@ -17,6 +55,20 @@ class TopNavbar extends React.Component {
         return (
             <>
                 <div id="top-navbar">
+                    <div className="top-navbar-menu-toggle">
+                        <div className="toggle-span"></div>
+                        <div className="toggle-span"></div>
+                        <div className="toggle-span"></div>
+
+                    </div>
+                    <div className="top-navbar-menu">
+                        <ul>
+                            <li>About Us</li>
+                            <li>Help</li>
+                            <li>Your Account</li>
+                            <li>Your Basket</li>
+                        </ul>
+                    </div>
                     <div className="company-offers">
                         <a href="/">Try our meal boxes</a>
                         <a href="/">100% organic</a>
@@ -24,6 +76,10 @@ class TopNavbar extends React.Component {
                     </div>
                 </div>
                 <div id="top-navbar-toggle">^</div>
+
+                <div className="top-navbar-logo">
+                    <img src={houseLogo} alt="herb house" />
+                </div>
             </>
         )
     }
