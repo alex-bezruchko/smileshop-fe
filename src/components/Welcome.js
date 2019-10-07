b5d08e09ca5bb89ef58d8dbe0adf4745a5a34291
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-10-07 11:55:25 -0400

    ADD SEARCHFORM - add searchform block - import to Welcome module - add css to input - add changehandler to input`s state

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,22 @@
 import React from "react";
 import welcomeLogo from "./../img/welcome.png";
+import SearchForm from "./../parts/SearchForm";
 class Welcome extends React.Component {
 
     componentDidMount() {
-        const welcomeDiv = document.getElementsByClassName('welcome-wrapper')[0];
+        const welcomeDiv = document.getElementsByClassName('header')[0];
         console.log(welcomeDiv)
         welcomeDiv.style.backgroundImage = `url(${welcomeLogo})`
     }
     render() {
         return (
             <div className="welcome-wrapper">
-                <h1 className="anton">Our Products</h1>
-                <h2 className="slab">Organic supplements grown with love and care</h2>
+                <div className="header">
+                    <h1 className="anton">Our Products</h1>
+                    <h2 className="slab">Organic supplements grown with love and care</h2>
+                </div>
                 {/* <img src={welcomeLogo} alt="welcome" className="welcome-img" /> */}
-
+                <SearchForm />
 
             </div>
 
